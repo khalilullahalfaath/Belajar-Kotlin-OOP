d6588da86f9c23f765dd414ed7fa86eca02ad836
@khalilullah.alfaath21 <khalilullah.alfaath21@gmail.com>
2023-02-10 22:09:36 +0800

    add all files

--- src/main/kotlin/app/Reflection.kt
+++ src/main/kotlin/app/Reflection.kt
@@ -4,17 +4,18 @@ import annotations.NotBlank
 import data.CreateCategoryRequest
 import data.CreateProductRequest
 import exception.ValidationException
-// import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.memberProperties
 
-/*
+
 fun validateRequest(request: Any){
     val clazz = request::class
-    val properties = clazz.members
+    val properties = clazz.memberProperties
 
     // iterate satu persatu, lalu cek kalau ada annotation @notBlank
     for(property in properties){
         if(property.findAnnotation<NotBlank>() != null){
-            val value: String = property.call(request)
+            val value = property.call(request)
             when(value){
                 is String ->{
                     if(value == ""){
@@ -24,15 +25,12 @@ fun validateRequest(request: Any){
             }
 
 
-    }
+        }
     }
 }
 
- */
-/*
 fun main() {
     val request = CreateProductRequest("1","Indomie", 2000)
     validateRequest(request)
-
 }
-*/
+
